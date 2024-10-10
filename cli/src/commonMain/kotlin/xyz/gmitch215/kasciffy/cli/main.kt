071cabbb82f5b2d5ed0a847d83ceef5cee6457e4
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-10-10 11:12:02 -0500

    Draft CLI Commands

---
+++ cli/src/commonMain/kotlin/xyz/gmitch215/kasciffy/cli/main.kt
@@ -0,0 +1,11 @@
+package xyz.gmitch215.kasciffy.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+
+class Kasciffy : CliktCommand(name = "kasciffy") {
+    override fun run() = Unit
+}
+
+fun main(args: Array<String>) = Kasciffy()
+    .main(args)
