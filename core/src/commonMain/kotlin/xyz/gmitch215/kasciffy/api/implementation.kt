bb1ec0d18325bc85a93ca2862d6c36deccac4622
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-01-12 15:04:58 -0600

    Fix `asciffy` Implementation
    - Add `downScale` Parameter
    - Add `asciffy` for Animated Media

--- core/src/commonMain/kotlin/xyz/gmitch215/kasciffy/api/implementation.kt
+++ core/src/commonMain/kotlin/xyz/gmitch215/kasciffy/api/implementation.kt
@@ -18,24 +18,28 @@ const val BLUE_GRAYSCALE = 0.5
  */
 const val GREEN_GRAYSCALE = 0.16
 
-internal suspend fun asciffy(image: Image, map: String): String = coroutineScope {
-    val width = image.width
-    val height = image.height
+internal suspend fun asciffy(image: Image, map: String, downScale: Int? = null): String = coroutineScope {
+    val downScale0 = downScale ?: ((image.width * image.height) / (64 * 2))
 
-    val ascii = Array(width) { CharArray(height) }
+    val width = image.width / downScale0
+    val height = image.height / downScale0
+
+    if (width == 0 || height == 0) return@coroutineScope ""
+
+    val ascii = Array(height) { CharArray(width) }
 
     coroutineScope {
         for (x in 0 until width)
             launch {
                 for (y in 0 until height) {
                     launch {
-                        val red = image.red(x, y)
-                        val green = image.green(x, y)
-                        val blue = image.blue(x, y)
+                        val red = image.red(x * downScale0, y * downScale0)
+                        val green = image.green(x * downScale0, y * downScale0)
+                        val blue = image.blue(x * downScale0, y * downScale0)
                         val gray = (red * RED_GRAYSCALE + green * GREEN_GRAYSCALE + blue * BLUE_GRAYSCALE).toInt()
 
                         val index = (gray * (map.length - 1)) / 255
-                        ascii[x][y] = map[index]
+                        ascii[y][x] = map[index]
                     }
                 }
             }
@@ -44,4 +48,20 @@ internal suspend fun asciffy(image: Image, map: String): String = coroutineScope
     return@coroutineScope ascii.joinToString("\n") {
         it.joinToString("")
     }
+}
+
+internal suspend fun asciffy(animation: AnimatedMedia, map: String, downScale: Int? = null): List<String> = coroutineScope {
+    val frames = animation.frames
+    val asciiFrames = Array(frames.size) { "" }
+    println("Frames: ${frames.size}")
+
+    coroutineScope {
+        for (i in frames.indices)
+            launch {
+                asciiFrames[i] = asciffy(frames[i], map, downScale)
+                println("${asciiFrames[i]}\n")
+            }
+    }
+
+    return@coroutineScope asciiFrames.toList()
 }
