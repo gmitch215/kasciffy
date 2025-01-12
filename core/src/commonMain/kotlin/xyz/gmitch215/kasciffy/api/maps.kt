4cfca9725579ffe6bb7842d4b762a37c88bcabcc
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-01-12 15:05:07 -0600

    Add More ASCII Maps

--- core/src/commonMain/kotlin/xyz/gmitch215/kasciffy/api/maps.kt
+++ core/src/commonMain/kotlin/xyz/gmitch215/kasciffy/api/maps.kt
@@ -1,19 +1,46 @@
+@file:Suppress("SpellCheckingInspection")
+
 package xyz.gmitch215.kasciffy.api
+
+/**
+ * An ASCII options map with fifty levels of detail.
+ */
+const val FIFTY = "@\$B%8&MW#*oakbdpqwmQ0CLJnurxyvzc[]{}()|\\/?!Iil+<>_:;~-^,`'. "
+
+/**
+ * An ASCII options map with thirty levels of detail.
+ */
+const val THIRTY = "@$#%&*o+=x^~;:-,.`\"|\\/[]{}()<>?_ "
+
+/**
+ * An ASCII options map with twenty levels of detail.
+ */
+const val TWENTY = "@&$%#*o+=x^~;:-,.` "
+
+/**
+ * An ASCII options map with fifteen levels of detail.
+ */
+const val FIFTEEN = "@&$%#0ol+=-:. "
 
 /**
  * An ASCII options map with twelve levels of detail.
  */
-const val TWELEVE = "@#$%!+=-:. "
+const val TWELVE = "@&$%#=+~-_. "
 
 /**
  * An ASCII options map with ten levels of detail.
  */
-const val TEN = "@#%!+=-:. "
+const val TEN = "@&$%#=+_. "
+
+/**
+ * An ASCII options map with ten levels of detail in numeric form.
+ */
+const val TEN_NUMERIC = "098654271 "
 
 /**
  * An ASCII options map with five levels of detail.
  */
-const val FIVE = "@=:. "
+const val FIVE = "@#$. "
 
 /**
  * An ASCII options map with five levels of detail in block form.
