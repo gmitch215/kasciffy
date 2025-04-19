df43e55fcdd3b0021bfc406545af0c7bb7c69e18
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-04-18 20:16:01 -0500

    update JsExport everywhere

--- core/src/common/main/dev/gmitch215/kasciffy/api/implementation.kt
+++ core/src/common/main/dev/gmitch215/kasciffy/api/implementation.kt
@@ -1,7 +1,6 @@
 @file:OptIn(ExperimentalJsFileName::class, ExperimentalJsExport::class)
 
 @file:JvmName("Kasciffy")
-@file:JsExport
 @file:JsFileName("kasciffy")
 
 package dev.gmitch215.kasciffy.api
@@ -18,18 +17,31 @@ import kotlin.jvm.JvmName
 /**
  * The scale value for the red channel in the RGB color space.
  */
+@JsExport
 const val RED_GRAYSCALE = 0.34
 
 /**
  * The scale value for the blue channel in the RGB color space.
  */
+@JsExport
 const val BLUE_GRAYSCALE = 0.5
 
 /**
  * The scale value for the green channel in the RGB color space.
  */
+@JsExport
 const val GREEN_GRAYSCALE = 0.16
 
+/**
+ * Asynchronously asciffies this image.
+ * @param map The ASCII map to use for asciffying.
+ * @param downScale The downscale factor to use when asciffying. This is used to reduce the size of the media, if necessary.
+ * If not specified, this is automatically calculated.
+ * @return The asciffied version of this image.
+ */
+suspend fun Image.asciffyAsString(map: String, downScale: Int?): String = asciffy0(this, map, downScale)
+
+@JsName("asciffy0_image")
 internal suspend fun asciffy0(image: Image, map: String, downScale: Int?? = null): String = coroutineScope {
     val downScale0 = downScale ?: ((image.width * image.height) / (64 * 2))
 
@@ -62,6 +74,17 @@ internal suspend fun asciffy0(image: Image, map: String, downScale: Int?? = null
         it.joinToString("")
     }
 }
+
+/**
+ * Asynchronously asciffies this animated media.
+ *
+ * This is not guaranteed to be available on all platforms.
+ * @param map The ASCII map to use for asciffying.
+ * @param downScale The downscale factor to use when asciffying. This is used to reduce the size of the media, if necessary.
+ * If not specified, this is automatically calculated.
+ * @return The asciffied version of this animated media.
+ */
+suspend fun AnimatedMedia.asciffyAsString(map: String, downScale: Int?): List<String> = asciffy0(this, map, downScale)
 
 @JsName("asciffy0_animated")
 internal suspend fun asciffy0(animation: AnimatedMedia, map: String, downScale: Int?? = null): List<String> = coroutineScope {
